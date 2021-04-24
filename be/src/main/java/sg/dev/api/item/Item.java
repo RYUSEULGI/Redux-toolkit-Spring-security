db41db144c1cb369cb7ab469d2a65dfc62153b9e
RYUSEULGI <rhythm296@naver.com>
2021-04-24 18:34:14 +0900

    폴더구조 추가

---
+++ be/src/main/java/sg/dev/api/item/Item.java
@@ -0,0 +1,2 @@
+package sg.dev.api.item;public class Item {
+}
---
+++ fe/src/item/component/ItemCreate.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import CreateForm from './CreateForm';
+
+const ItemCreate = () => (
+    <>
+        <h1>Create</h1>
+        <CreateForm />
+    </>
+);
+
+export default ItemCreate;
